165cbd90c7edcfc68f45dd094d3cf4afbeb98077
Deniss Simonaits <dsimonaits@gmail.com>
2022-11-26 18:47:33 +0200

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { List } from './FeedbackOptions.styled';
 import { ListItem } from './FeedbackOptions.styled';
+import { FeedBackBtn } from './FeedbackOptions.styled';
 
 const FeedbackOptions = ({ options, onFeedbackClick }) => {
   const optionsArr = Object.keys(options);
@@ -8,12 +9,27 @@ const FeedbackOptions = ({ options, onFeedbackClick }) => {
     <List>
       {optionsArr.map(option => {
         const buttonName = option[0].toUpperCase() + option.slice(1);
-
+        const color = option => {
+          if (option === 'good') {
+            return 'green';
+          }
+          if (option === 'neutral') {
+            return 'yellow';
+          }
+          if (option === 'bad') {
+            return 'red';
+          }
+        };
         return (
           <ListItem key={option}>
-            <button tybe="button" onClick={() => onFeedbackClick(option)}>
+            <FeedBackBtn
+              name=" {buttonName}"
+              color={color(option)}
+              type="button"
+              onClick={() => onFeedbackClick(option)}
+            >
               {buttonName}
-            </button>
+            </FeedBackBtn>
           </ListItem>
         );
       })}
